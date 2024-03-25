62c5c6a19a49f1d5679110d76e78a23160d4bffe
alexweinstein <alex.weinstein@improving.com>
2024-03-25 11:08:52 -0400

    tests & all functions filled out in server

--- server/src/main/scala/com/ossuminc/riddl/lsp/server/RiddlLSPServer.scala
+++ server/src/main/scala/com/ossuminc/riddl/lsp/server/RiddlLSPServer.scala
@@ -1,34 +1,29 @@
 package com.ossuminc.riddl.lsp.server
 
+import com.ossuminc.riddl.lsp.utils.createInitializeResultIncremental
 import org.eclipse.lsp4j.{CompletionOptions, InitializeParams, InitializeResult, ServerCapabilities, TextDocumentSyncKind}
 import org.eclipse.lsp4j.services.{LanguageServer, TextDocumentService, WorkspaceService}
 
 import java.util.concurrent.CompletableFuture
 
 class RiddlLSPServer extends LanguageServer {
-  var errorCode = 0
-
   val documentService = new RiddlLSPTextDocumentService()
   val workspaceService = new RiddlLSPWorkspaceService()
 
   override def initialize(params: InitializeParams): CompletableFuture[InitializeResult] = {
-    val result = new InitializeResult(new ServerCapabilities())
-
-    result.getCapabilities.setTextDocumentSync(TextDocumentSyncKind.Full)
-    result.getCapabilities.setCompletionProvider(new CompletionOptions())
+    val result = createInitializeResultIncremental()
     CompletableFuture.supplyAsync(() => result)
   }
 
   override def shutdown(): CompletableFuture[AnyRef] = {
-    errorCode = 0
-    null
+    CompletableFuture.supplyAsync(() => null)
   }
 
   override def exit(): Unit = {
-    System.exit(errorCode)
+    ()
   }
 
-  override def getTextDocumentService: TextDocumentService = ???
+  override def getTextDocumentService: TextDocumentService = new RiddlLSPTextDocumentService()
 
-  override def getWorkspaceService: WorkspaceService = ???
+  override def getWorkspaceService: WorkspaceService = new RiddlLSPWorkspaceService()
 }
