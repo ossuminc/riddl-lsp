443624900dd93961359eea158f8db28932080f8c
alexweinstein <alex.weinstein@improving.com>
2024-04-02 08:33:24 -0400

    didOpen, didClose, didChange

--- server/src/main/scala/com/ossuminc/riddl/lsp/server/RiddlLSPTextDocumentService.scala
+++ server/src/main/scala/com/ossuminc/riddl/lsp/server/RiddlLSPTextDocumentService.scala
@@ -1,13 +1,21 @@
 package com.ossuminc.riddl.lsp.server
 
 import org.eclipse.lsp4j.jsonrpc.messages
-import org.eclipse.lsp4j.{CompletionItem, CompletionList, CompletionParams, DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams, DidSaveTextDocumentParams}
+import org.eclipse.lsp4j.{CompletionItem, CompletionList, CompletionParams, DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams, DidSaveTextDocumentParams, TextDocumentContentChangeEvent}
 import org.eclipse.lsp4j.services.TextDocumentService
 
 import java.util
 import java.util.concurrent.CompletableFuture
+import scala.jdk.CollectionConverters.*
+
+case class RiddlCodeDoc(textString: String = "")
+
+object RiddlCodeDoc {
+}
 
 class RiddlLSPTextDocumentService extends TextDocumentService {
+  private var riddlDoc: RiddlCodeDoc = RiddlCodeDoc()
+  private val unsavedChanges: Seq[TextDocumentContentChangeEvent] = Seq()
 
   override def completion(position: CompletionParams):
     CompletableFuture[messages.Either[util.List[CompletionItem], CompletionList]] = {
@@ -18,11 +26,17 @@ class RiddlLSPTextDocumentService extends TextDocumentService {
       }
     )
   }
-  override def didOpen(params: DidOpenTextDocumentParams): Unit = ???
+  override def didOpen(params: DidOpenTextDocumentParams): Unit = {
+    riddlDoc = RiddlCodeDoc(params.getTextDocument.getText)
+  }
 
-  override def didChange(params: DidChangeTextDocumentParams): Unit = ???
+  override def didChange(params: DidChangeTextDocumentParams): Unit = {
+    unsavedChanges ++ params.getContentChanges.asScala
+  }
 
-  override def didClose(params: DidCloseTextDocumentParams): Unit = ???
+  override def didClose(params: DidCloseTextDocumentParams): Unit = {
+    riddlDoc = RiddlCodeDoc()
+  }
 
   override def didSave(params: DidSaveTextDocumentParams): Unit = ???
 }
