6e9c5066f88a750a8466c710048e0de94c4629eb
alexweinstein <alex.weinstein@improving.com>
2024-04-09 16:14:39 -0400

    didOpen everything.riddl test passing with completion [OTOT 10-13]

--- server/src/main/scala/com/ossuminc/riddl/lsp/server/RiddlLSPTextDocumentService.scala
+++ server/src/main/scala/com/ossuminc/riddl/lsp/server/RiddlLSPTextDocumentService.scala
@@ -1,18 +1,35 @@
 package com.ossuminc.riddl.lsp.server
 
-import com.ossuminc.riddl.language.AST
+import com.ossuminc.riddl.language.{AST, Messages}
 import com.ossuminc.riddl.language.Messages.Messages
-import com.ossuminc.riddl.language.parsing.{SourceParserInput, StringParserInput, TopLevelParser}
+import com.ossuminc.riddl.language.parsing.{
+  SourceParserInput,
+  StringParserInput,
+  TopLevelParser
+}
+import com.ossuminc.riddl.lsp.utils.implicits.*
 import org.eclipse.lsp4j
 import org.eclipse.lsp4j.jsonrpc.messages
-import org.eclipse.lsp4j.{CompletionItem, CompletionList, CompletionParams, DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams, DidSaveTextDocumentParams, Position, TextDocumentContentChangeEvent}
+import org.eclipse.lsp4j.{
+  CompletionItem,
+  CompletionList,
+  CompletionParams,
+  DidChangeTextDocumentParams,
+  DidCloseTextDocumentParams,
+  DidOpenTextDocumentParams,
+  DidSaveTextDocumentParams,
+  Position
+}
 import org.eclipse.lsp4j.services.TextDocumentService
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import java.util
 import java.util.concurrent.CompletableFuture
+import scala.concurrent.Future
 import scala.io.Source
 import scala.jdk.CollectionConverters.*
+import scala.jdk.FutureConverters.*
+import scala.xml.include.UnavailableResourceException
 
 def getRootFromUri(uri: String) = {
   uri.split("/riddl/").drop(-1).mkString + "riddl/"
@@ -29,11 +46,21 @@ def parseDocFromString(docString: String): Either[Messages, AST.Root] =
 class RiddlLSPTextDocumentService extends TextDocumentService {
   private var docURI: Option[String] = None
   private var riddlDoc: Option[String] = None
-  private var parsedDoc: Option[Either[Messages, AST.Root]] = None
+  private var docLines: Seq[String] = Seq()
+  private var docAST: Option[Either[Messages, AST.Root]] = None
+
+  // Updating Vars
+  private def updateDocLines(): Unit = {
+    docLines =
+      if riddlDoc.isDefined then riddlDoc.getOrElse("").getLinesFromText
+      else Seq()
+  }
 
   private def updateParsedDoc(fromURI: Boolean = true): Unit = {
-    if fromURI then parsedDoc = riddlDoc.map(parseDocFromString)
-    else parsedDoc = docURI.map(parseDocFromSource)
+    if fromURI then docAST = riddlDoc.map(parseDocFromString)
+    else docAST = docURI.map(parseDocFromSource)
+
+    updateDocLines()
   }
 
   private def updateRIDDLDocFromURI(): Unit = docURI.foreach(uri => {
@@ -41,21 +68,82 @@ class RiddlLSPTextDocumentService extends TextDocumentService {
     lazy val data: String =
       try {
         source.mkString
-      }
-      finally {
+      } finally {
         source.close()
       }
     riddlDoc = if data.nonEmpty then Some(data) else None
   })
 
-  override def completion(position: CompletionParams):
-    CompletableFuture[messages.Either[util.List[CompletionItem], CompletionList]] = {
-      CompletableFuture.supplyAsync(() => {
-        org.eclipse.lsp4j.jsonrpc.messages.Either.forLeft(
-          java.util.List.of()
+  // Functionality
+  override def completion(position: CompletionParams): CompletableFuture[
+    messages.Either[util.List[CompletionItem], CompletionList]
+  ] = {
+    val ast: Either[Messages, AST.Root] =
+      if !docURI.contains(position.getTextDocument.getUri) then
+        parseDocFromSource(position.getTextDocument.getUri)
+      else docAST.getOrElse(Right(AST.Root()))
+
+    if ast.isRight
+    then
+      Future.failed(UnavailableResourceException()).asJava.toCompletableFuture
+    else {
+      val completionsOpt = getCompletionFromAST(
+        position.getPosition.getLine,
+        position.getPosition.getCharacter
+      )
+      if completionsOpt.isDefined then
+        Future
+          .successful(
+            completionsOpt.getOrElse(messages.Either.forLeft(Seq().asJava))
+          )
+          .asJava
+          .toCompletableFuture
+      else Future.failed(Throwable()).asJava.toCompletableFuture
+    }
+  }
+
+  private def getCompletionFromAST(
+      line: Int,
+      charPosition: Int
+  ): Option[messages.Either[util.List[CompletionItem], CompletionList]] = {
+    var completions = CompletionList()
+    if docAST.isDefined then
+      if docAST.exists(_.isRight) then None
+      else {
+        docAST.map(
+          _.fold(
+            msgs => {
+              val completionList = new CompletionList()
+              val grouped = msgs
+                .groupBy(msg => msg.loc.line)
+              val lineItems =
+                if grouped.isDefinedAt(line) then Some(grouped(line)) else None
+              val selectedItem: Seq[Messages.Message] =
+                lineItems
+                  .map { items =>
+                    val filtered = items.filterNot(_.loc.offset == charPosition)
+                    if filtered.nonEmpty then filtered else Seq()
+                  }
+                  .getOrElse(Seq())
+
+              completionList.setItems(
+                selectedItem
+                  .map(msg => {
+                    val item = new CompletionItem()
+                    item.setTextEditText(msg.message)
+                    item
+                  })
+                  .asJava
+              )
+              messages.Either.forRight(
+                completionList
+              )
+            },
+            _ => messages.Either.forLeft(Seq[CompletionItem]().asJava)
+          )
         )
       }
-    )
+    else None
   }
 
   override def didOpen(params: DidOpenTextDocumentParams): Unit = {
@@ -74,25 +162,28 @@ class RiddlLSPTextDocumentService extends TextDocumentService {
     riddlDoc = riddlDoc.map(doc =>
       var docLines: Seq[String] = doc.linesIterator.toSeq
       val changes = params.getContentChanges.asScala.toSeq
-      if docLines.nonEmpty then changes.foreach(change => {
-        val changeRangeStart: Position = change.getRange.getStart
-        val changeRangeEnd: Position = change.getRange.getEnd
-        val changesToPatch: Seq[String] = docLines.slice(
-          changeRangeStart.getLine,
-          changeRangeEnd.getLine
-        )
-        val changeLines = change.getText.linesIterator.toSeq
-        val startLinePatch: String = patchLine(docLines.head, change.getRange, changeLines.head)
-        val middlePatch: Seq[String] = changeLines.slice(1, -1)
-        val endLinePatch: String = patchLine(docLines.last, change.getRange, changeLines.last)
+      if docLines.nonEmpty then
+        changes.foreach(change => {
+          val changeRangeStart: Position = change.getRange.getStart
+          val changeRangeEnd: Position = change.getRange.getEnd
+          val changesToPatch: Seq[String] = docLines.slice(
+            changeRangeStart.getLine,
+            changeRangeEnd.getLine
+          )
+          val changeLines = change.getText.getLinesFromText
+          val startLinePatch: String =
+            patchLine(docLines.head, change.getRange, changeLines.head)
+          val middlePatch: Seq[String] = changeLines.slice(1, -1)
+          val endLinePatch: String =
+            patchLine(docLines.last, change.getRange, changeLines.last)
 
-        val finalPatch = startLinePatch +: middlePatch :+ endLinePatch
-        docLines = docLines.patch(
-          changeRangeStart.getLine, //start of replacement
-          startLinePatch +: middlePatch :+ endLinePatch,
-          changeRangeEnd.getLine - changeRangeStart.getLine //length to replace (will be deleted)
-        )
-      })
+          val finalPatch = startLinePatch +: middlePatch :+ endLinePatch
+          docLines = docLines.patch(
+            changeRangeStart.getLine, // start of replacement
+            startLinePatch +: middlePatch :+ endLinePatch,
+            changeRangeEnd.getLine - changeRangeStart.getLine // length to replace (will be deleted)
+          )
+        })
       else docLines = Seq(changes.map(_.getText).mkString("\n"))
       docLines.mkString
     )
@@ -108,4 +199,20 @@ class RiddlLSPTextDocumentService extends TextDocumentService {
     updateRIDDLDocFromURI()
     updateParsedDoc()
   }
+
+  // TODO: uncomment and finish when appropriate
+  /*
+  override def references(
+      params: ReferenceParams
+  ): CompletableFuture[util.List[_ <: Location]] = {
+    var foundLocations: Seq[Location] = Seq()
+    riddlDoc.foreach(doc => {
+      val docLines: Seq[String] = doc.linesIterator.toSeq
+
+    })
+    if riddlDoc.isDefined then completableFutureWithSeq(foundLocations)
+    else completableFutureWithSeq(Seq[Location]())
+  }
+   */
+
 }
--- server/src/test/scala/com/ossum/riddl/lsp/server/RiddlLSPTextDocumentSpec.scala
+++ server/src/test/scala/com/ossum/riddl/lsp/server/RiddlLSPTextDocumentSpec.scala
@@ -1,48 +1,72 @@
 package com.ossum.riddl.lsp.server
 
-import com.ossuminc.riddl.lsp.server.{RiddlLSPServer, RiddlLSPTextDocumentService, RiddlLSPWorkspaceService}
-import com.ossuminc.riddl.lsp.utils.createInitializeResultIncremental
+import com.ossuminc.riddl.lsp.server.RiddlLSPTextDocumentService
 import org.eclipse.lsp4j
-import org.eclipse.lsp4j.{DidChangeTextDocumentParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams, DidSaveTextDocumentParams, InitializeParams, InitializeResult, Position, TextDocumentContentChangeEvent, TextDocumentIdentifier, TextDocumentItem, VersionedTextDocumentIdentifier}
-import org.mockito.Mockito.mock
+import org.eclipse.lsp4j.jsonrpc.messages
+import org.eclipse.lsp4j.{
+  CompletionItem,
+  CompletionList,
+  CompletionParams,
+  DidChangeTextDocumentParams,
+  DidCloseTextDocumentParams,
+  DidOpenTextDocumentParams,
+  DidSaveTextDocumentParams,
+  Position,
+  TextDocumentContentChangeEvent,
+  TextDocumentIdentifier,
+  TextDocumentItem,
+  VersionedTextDocumentIdentifier
+}
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 import java.nio.file.Path
+import java.util
 import scala.io.{BufferedSource, Source}
 import scala.language.postfixOps
 import scala.jdk.CollectionConverters.*
+import scala.jdk.FutureConverters.*
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class RiddlLSPTextDocumentSpec extends AnyWordSpec with Matchers {
-  val filePath = "server/src/test/resources/everything.riddl"
-  val file: BufferedSource = Source.fromFile(filePath)
-  val doc: String = file.getLines().mkString
-  file.close()
-  val docURI: String = Path.of(filePath).toUri.toString
+  trait EverythingFileSpec {
+    val filePath = "server/src/test/resources/everything.riddl"
+    val file: BufferedSource = Source.fromFile(filePath)
+    val doc: String = file.getLines().mkString
+    file.close()
+    val docURI: String = Path.of(filePath).toUri.toString
+  }
 
-  trait EverythingInitializeSpec {
+  trait EverythingInitializeSpec extends EverythingFileSpec {
+
     val textDocumentItem: TextDocumentItem = new TextDocumentItem()
     textDocumentItem.setText(doc)
     textDocumentItem.setUri(docURI)
-    val textDocumentIdentifier: TextDocumentIdentifier = new TextDocumentIdentifier()
+    val textDocumentIdentifier: TextDocumentIdentifier =
+      new TextDocumentIdentifier()
     textDocumentIdentifier.setUri(docURI)
 
     val service: RiddlLSPTextDocumentService = RiddlLSPTextDocumentService()
   }
 
   trait EmptyInitializeSpec {
-    val emptyDocURI: String = Path.of("server/src/test/resources/empty.riddl").toUri.toString
+    val emptyDocURI: String =
+      Path.of("server/src/test/resources/empty.riddl").toUri.toString
+
     val textDocumentItem: TextDocumentItem = new TextDocumentItem()
     textDocumentItem.setText("")
     textDocumentItem.setUri(emptyDocURI)
-    val textDocumentIdentifier: TextDocumentIdentifier = new TextDocumentIdentifier()
+    val textDocumentIdentifier: TextDocumentIdentifier =
+      new TextDocumentIdentifier()
     textDocumentIdentifier.setUri(emptyDocURI)
 
     val service: RiddlLSPTextDocumentService = RiddlLSPTextDocumentService()
   }
 
   trait OpenEverythingFileSpec extends EverythingInitializeSpec {
-    val openNotification: DidOpenTextDocumentParams = new DidOpenTextDocumentParams()
+    val openNotification: DidOpenTextDocumentParams =
+      new DidOpenTextDocumentParams()
     openNotification.setTextDocument(
       textDocumentItem
     )
@@ -51,7 +75,8 @@ class RiddlLSPTextDocumentSpec extends AnyWordSpec with Matchers {
   }
 
   trait OpenEmptyFileSpec extends EmptyInitializeSpec {
-    val openNotification: DidOpenTextDocumentParams = new DidOpenTextDocumentParams()
+    val openNotification: DidOpenTextDocumentParams =
+      new DidOpenTextDocumentParams()
     openNotification.setTextDocument(
       textDocumentItem
     )
@@ -60,7 +85,8 @@ class RiddlLSPTextDocumentSpec extends AnyWordSpec with Matchers {
   }
 
   trait ChangeEverythingFileSpec extends OpenEverythingFileSpec {
-    val changeNotification: DidChangeTextDocumentParams = new DidChangeTextDocumentParams()
+    val changeNotification: DidChangeTextDocumentParams =
+      new DidChangeTextDocumentParams()
 
     val versionedDocIdentifier = new VersionedTextDocumentIdentifier()
     versionedDocIdentifier.setUri(docURI)
@@ -86,8 +112,9 @@ class RiddlLSPTextDocumentSpec extends AnyWordSpec with Matchers {
     service.didChange(changeNotification)
   }
 
-  trait ChangeEmptyFileSpec extends OpenEmptyFileSpec {
-    val changeNotification: DidChangeTextDocumentParams = new DidChangeTextDocumentParams()
+  trait ChangeEmptyFileSpec extends OpenEmptyFileSpec with EverythingFileSpec {
+    val changeNotification: DidChangeTextDocumentParams =
+      new DidChangeTextDocumentParams()
 
     val versionedDocIdentifier = new VersionedTextDocumentIdentifier()
     versionedDocIdentifier.setUri(docURI)
@@ -114,15 +141,34 @@ class RiddlLSPTextDocumentSpec extends AnyWordSpec with Matchers {
   }
 
   "RiddlLSPTextDocumentService" must {
-    "successfully open everything.riddl" in new OpenEverythingFileSpec {}
+    "successfully open everything.riddl & get completion" in new OpenEverythingFileSpec {
+      val params = new CompletionParams()
+      params.setTextDocument(textDocumentIdentifier)
+      val position = new Position()
+      position.setLine(1)
+      position.setCharacter(0)
+      params.setPosition(position)
+      val resultF
+          : Future[messages.Either[util.List[CompletionItem], CompletionList]] =
+        service.completion(params).asScala
+      resultF.map { eitherList =>
+        eitherList.isRight mustBe true
+        eitherList.getRight.getItems
+          .get(
+            0
+          )
+          .getTextEditText mustEqual """"Expected one of ("/*" | "//" | "adaptor" | "by" | "command" | "connector" | "entity" | "event" | "flow" | "function" | "graph" | "handler" | "include" | "inlet" | "merge" | "option" | "outlet" | "projector" | "query" | "record" | "repository" | "result" | "router" | "saga" | "sink" | "source" | "split" | "table" | "term" | "type" | "void" | "}")""""
+      }
+    }
 
     "successfully close everything.riddl" in new OpenEverythingFileSpec {
-      val closeNotification: DidCloseTextDocumentParams = new DidCloseTextDocumentParams()
+      val closeNotification: DidCloseTextDocumentParams =
+        new DidCloseTextDocumentParams()
       closeNotification.setTextDocument(textDocumentIdentifier)
       service.didClose(closeNotification)
     }
 
-    "successfully change everything.riddl" in new ChangeEverythingFileSpec { }
+    "successfully change everything.riddl" in new ChangeEverythingFileSpec {}
 
     "successfully save everything.riddl" in new ChangeEverythingFileSpec {
       val saveNotification = new DidSaveTextDocumentParams()
